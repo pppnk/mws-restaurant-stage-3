d779665810fbd961ed470927514bf9311c75392e
pppnk <pppnk@hotmail.com>
2019-06-15 01:32:42 -0700

    fix:Service worker
    Service worker fix

--- js/service_worker.js
+++ js/service_worker.js
@@ -1,10 +1,54 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('/sw.js', {scope: "/"})
-        .then(reg => {
-            console.log('Service Worker Registration Successful: ' + reg.scope);
+const cacheVersion = 'mws-restaurant-static-v-2';
+
+
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(cacheVersion).then(function(cache) {
+            return cache.addAll([
+                "/",
+                "index.html",
+                "restaurant.html",
+                "/css/main.css",
+                "/css/responsive.css",
+                "/js/dbhelper.js",
+                "/js/main.js",
+                "/js/restaurant_info.js",
+                "/js/idb_util.js",
+                "/js/service_worker.js",
+                "/img/*"
+            ]).catch(error => {});
         })
-        .catch(error => {
-            console.log('Service Worker Registration Failed: ' + error);
-        });
-}
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('mws-restaurant-') && cacheName !== cacheVersion;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith (
+        caches.match(event.request).then(function(response) {
+            if (response !== undefined) {
+                return response;
+            } else {
+                return fetch(event.request).then(function (response) {
+                    let responseClone = response.clone();
+                    caches.open(cacheVersion).then(function (cache) {
+                        cache.put(event.request, responseClone);
+                    });
+                    return response;
+                });
+            }
+        })
+    );
+});
